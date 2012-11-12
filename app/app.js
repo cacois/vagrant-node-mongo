665ba09e196b8b112dd2c543fccd35c07d938085
Aaron Cois <aaron.cois@gmail.com>
2012-11-11 21:08:10 -0500

    Updating app html

--- app/app.js
+++ app/app.js
@@ -1,8 +1,8 @@
 var http = require('http');
 
 server = http.createServer(function(req, res){
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.write("Hello, Showclix!");
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write("<html><body><h1>Hello, World!</h1></body></html>");
     res.end();
 }).listen(3000);
 
